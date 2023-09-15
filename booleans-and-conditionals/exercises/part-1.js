b8897d0b477bf7c98947e406a32a43f9d8318b6d
timconceivable <headskape@yahoo.com>
2023-09-15 00:39:16 -0500

    exercise part 1

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,22 +1,21 @@
+const input = require('readline-sync');
+
 // Declare and initialize the variables for exercise 1 here:
-let engineIndicatorLight = "red blinking";
-let spaceSuitsOn = true;
-let shuttleCabinReady = true;
+let spaceSuitsOn = Boolean(true);
+let shuttleCabinReady = Boolean(true);
 let crewStatus = spaceSuitsOn && shuttleCabinReady;
-let computerStatusCode = 200;
-let shuttleSpeed = 15000;
-let fuelLevel = 20000;
-let engineTemp = 1000;
+let computerStatusCode = 400;
+//let shuttleSpeed = 15000;
+let fuelLevel = 21000; //Number(input.question("Fuel Level: "));
+let engineTemp = 1200; //Number(input.question("Engine Temp: "));
+let engineIndicator = "green blinking" //input.question("Engine Indicator Light: ");
+let commandOverride = Boolean(input.question("Override? (Y/N): ").toLowerCase() == "y") ? true : false;
+let overrideStatus = commandOverride ? "ACTIVE" : "INACTIVE";
+let launchReady = commandOverride;
 
 // BEFORE running the code, predict what will be printed to the console by the following statements:
+console.log(`System Override: ${overrideStatus}`);
 
-if (engineIndicatorLight === "green") {
-   console.log("engines have started");
-} else if (engineIndicatorLight === "green blinking") {
-   console.log("engines are preparing to start");
-} else {
-   console.log("engines are off");
-}
 if (crewStatus) {
    console.log("Crew Ready");
 } else {
@@ -31,6 +30,7 @@ if (computerStatusCode === 200) {
    console.log("ALERT: Computer offline!");
 }
 
+/* 
 if (shuttleSpeed > 17500) {
    console.log("ALERT: Escape velocity reached!");
 } else if (shuttleSpeed < 8000) {
@@ -38,31 +38,37 @@ if (shuttleSpeed > 17500) {
 } else {
    console.log("Stable speed");
 }
+*/
 
-if (fuelLevel < 1000 || engineTemp > 3500 || engineIndicatorLight === "red blinking") {
-   console.log("EMGOME FAILURE IMMINENT!");
-}
-
-if (fuelLevel > 20000 && engineTemp <= 2500) {
+if (fuelLevel < 1000 || engineTemp > 3500 || engineIndicator === "red blinking") {
+   console.log("ENGINE FAILURE IMMINENT!");
+} else if (fuelLevel <= 5000 || engineTemp > 2500) {
+   console.log("Check fuel level. Engines running hot.");
+} else if (fuelLevel > 20000 && engineTemp <= 2500) {
    console.log("Full tank. Engines good.");
+   if (engineIndicator != "red blinking") { 
+      launchReady = true; 
+   }
 } else if (fuelLevel > 10000 && engineTemp <= 2500) {
    console.log("Fuel level is above 50%. Engines good.");
-} else if (fuelLevel > 5000 && engineTemp <= 2500) {
+} else if (fuelLevel > 5000 || engineTemp <= 2500) {
    console.log("Fuel level is above 25%. Engines good.");
-else if (fuelLevel > 10000 && engineTemp <= 2500) {
-   console.log("Fuel level is above 50%. Engines good.");
-
-
-/*
-if (crewStatus && computerStatusCode === 200 && spaceSuitsOn) {
-   console.log("all systems go");
 } else {
-   console.log("WARNING. Not ready");
+   console.log("Fuel and engine status pending...");
 }
 
-if (!crewStatus || computerStatusCode !== 200 || !spaceSuitsOn) {
-   console.log("WARNING. Not ready");
+if (engineIndicator === "green") {
+   console.log("Engines have started");
+} else if (engineIndicator === "green blinking") {
+   console.log("Engines are preparing to start");
+} else if (engineIndicator === "red blinking") {
+   console.log("WARNING: Check engines!");
 } else {
-   console.log("all systems go");
+   console.log("Engines are off");
 }
-*/
+
+if (launchReady) {
+   console.log("Cleared to launch!");
+} else {
+   console.log("Launch scrubbed.");
+}
