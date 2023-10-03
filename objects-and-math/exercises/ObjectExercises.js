008df974ee0a9af7d4e0b2660baebd2593fdb939
timconceivable <headskape@yahoo.com>
2023-10-03 09:06:01 -0500

    ch.12 studio part 1

--- objects-and-math/exercises/ObjectExercises.js
+++ objects-and-math/exercises/ObjectExercises.js
@@ -20,3 +20,17 @@ let salamander = {
 // Print out the relevant information about each animal.
 
 // Start an animal race!
+
+
+let students = {
+   names:   ["G Darris", "B Cat", "A Dog", "M Zebra", "K Kiwi"],
+   grades:  [100, 95, 74, 82, 91]
+};
+ 
+for (student in students.names) {
+   console.log(students.names[student], ": ", students.grades[student]);
+}
+
+
+// console.log(Math.floor(1.9999999999999999999999999));
+// console.log( Math.round(Math.random()*1000)/10  );
--- objects-and-math/studio/ObjectsStudio01.js
+++ objects-and-math/studio/ObjectsStudio01.js
@@ -1,12 +1,37 @@
-// Code your selectRandomEntry function here:
-
-
-// Code your buildCrewArray function here:
-
+const input = require('readline-sync');
 
 let idNumbers = [291, 414, 503, 599, 796, 890];
 
-// Here are the candidates and the 'animals' array:
+// Code your selectRandomId function here:
+function selectRandomId(idArray) {
+  return idArray[Math.floor(Math.random()*idArray.length)];
+}
+
+function selectCrewIds(num) {
+  let crewIds = [];
+  while (crewIds.length < num) {
+    const selectedId = selectRandomId(idNumbers);
+    if (!crewIds.includes(selectedId)) {
+      crewIds.push(selectedId);
+    }
+  }
+  return crewIds;
+}
+
+// Code your buildCrewArray function here:
+function buildCrewArray(crewIds,candidatesList) {
+  let crewArray = [];
+  for (index in candidatesList) {
+    let animal = candidatesList[index];
+    if (crewIds.includes(animal.astronautID)) {
+      crewArray.push(animal);
+    }
+  }
+  return crewArray;
+}
+
+
+// Here are the candidates and the 'candidatesList' array:
 let candidateA = {
   'name':'Gordon Shumway',
   'species':'alf',
@@ -15,41 +40,70 @@ let candidateA = {
   'astronautID':414
 };
 let candidateB = {
-  'name':'Lassie',
+  'name':'Lassie Collie',
   'species':'dog',
   'mass':19.1,
   'o2Used':function(hrs){return 0.030*hrs},
   'astronautID':503
 };
 let candidateC = {
-  'name':'Jonsey',
+  'name':'Jonesy Ripley',
   'species':'cat',
   'mass':3.6,
   'o2Used':function(hrs){return 0.022*hrs},
   'astronautID':796
 };
 let candidateD = {
-  'name':'Paddington',
+  'name':'Paddington Bear',
   'species':'bear',
   'mass':31.8,
   'o2Used':function(hrs){return 0.047*hrs},
   'astronautID':291
 };
 let candidateE = {
-  'name':'Pete',
+  'name':'Master Oogway',
   'species':'tortoise',
   'mass':417,
   'o2Used':function(hrs){return 0.010*hrs},
   'astronautID':599
 };
 let candidateF = {
-  'name':'Hugs',
+  'name':'Hugs McGee',
   'species':'ball python',
   'mass':2.3,
   'o2Used':function(hrs){return 0.018*hrs},
   'astronautID':890
 };
 
-let animals = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+function makeNamesString(crewArray) {
+  names = "";
+  for (index in crewArray) {
+    if (crewArray.length > 1) {
+      if (index == crewArray.length-1) {
+        names += `and ${crewArray[index].name} are`;
+      } else {
+        names += `${crewArray[index].name}, `;
+        if (crewArray.length < 3) {
+          names = names.replace(",","");
+        }
+      }
+    } else names = `${crewArray[index].name} is`;
+  }
+  return names;
+}
 
-// Code your template literal and console.log statements:
+let candidatesList = [candidateA,candidateB,candidateC,candidateD,candidateE,candidateF];
+let candidatesNum = candidatesList.length;
+let userNum = Number(input.question("How many crew members are required? "));
+let abortMission = "n";
+if (userNum > candidatesNum) { 
+  userNum = candidatesNum; 
+  abortMission = input.question(`There are only ${candidatesNum} candidates available. The mission may not succeed without the proper number of crew members. Would you like to abort mission? `);
+}
+if (abortMission.toLowerCase() === "n") {  
+  let crew = buildCrewArray(selectCrewIds(userNum),candidatesList);
+  let namesString = makeNamesString(crew);
+
+  // Code your template literal and console.log statements:
+  console.log(`${namesString} going to space!`);
+} else console.log("MISSION ABORTED.")
