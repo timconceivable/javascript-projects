8dba140c22f90b03783bf10a0fbb7366e6c42522
timconceivable <headskape@yahoo.com>
2023-10-09 23:31:43 -0500

    ch.14 studio- unit testing

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,59 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput: function(num) {
+    let divisors = {
+      2: "Launch",
+      3: "Code",
+      5: "Rocks",
+    }
+    let message = ""
+    for (let i in divisors) {
+      if (num % i === 0) {
+        message += divisors[i];
+      }
+    }
+    if (message === "") {
+      message = "Rutabagas! That doesn't work."
+    }
+    if (!message.includes("!")) {
+      message += "!";
+    }
+    if (message.includes("hR")) {
+      message += " (CRASH!!!!)";
+    }
+    if (message.indexOf("R") > 0) {
+      message = message.replace("R"," R");
+    }
+
+    return message;
+  }
 }
 
-module.exports = launchcode;
+/* 
+for (i=0; i < 31; i++) {
+  console.log(i, launchcode.launchOutput(i));
+}
 
+let expectedOutput = {
+  1: "Rutabagas! That doesn't work.",
+  2: "Launch!",
+  3: "Code!",
+  5: "Rocks!",
+  6: "LaunchCode!",
+  10: "Launch Rocks! (CRASH!!!!)",
+  15: "Code Rocks!",
+  30: "LaunchCode Rocks!"
+};
+
+for (let i in expectedOutput) {
+  console.log(i);
+  console.log(expectedOutput.i);
+  console.log(expectedOutput[i]);
+}
+*/
+
+module.exports = launchcode;
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -1,8 +1,45 @@
 // launchcode.test.js code:
 const launchcode = require('../index.js');
 
+// Write your unit tests here!
 describe("Testing launchcode", function(){
 
-  // Write your unit tests here!
+  test("checks that the value of organization is 'nonprofit'", 
+  function() {
+    expect(launchcode.organization).toEqual("nonprofit");
+  });
+
+  test("checks that the value of executiveDirector is equal to 'Jeff'", function() {
+    expect(launchcode.executiveDirector).toEqual("Jeff");
+  });
+
+  test("checks that percentageCoolEmployees is equal to 100", function() {
+    expect(launchcode.percentageCoolEmployees).toEqual(100);
+  });
+
+  test("checks values of the programsOffered array and that it's length is 3", function() {
+    expect(launchcode.programsOffered).toContain("Web Development");
+    expect(launchcode.programsOffered).toContain("Data Analysis");
+    expect(launchcode.programsOffered).toContain("Liftoff");
+    expect(launchcode.programsOffered).toHaveLength(3);
+  });
+
+  let expectedOutput = {
+    1: "Rutabagas! That doesn't work.",
+    2: "Launch!",
+    3: "Code!",
+    5: "Rocks!",
+    6: "LaunchCode!",
+    10: "Launch Rocks! (CRASH!!!!)",
+    15: "Code Rocks!",
+    30: "LaunchCode Rocks!"
+  };
   
+  test("checks that the launchOutput function returns the correct strings", function() {
+    for (let i in expectedOutput) {
+      expect(launchcode.launchOutput(i)).toEqual(expectedOutput[i]); 
+    }
+  });
+
+
 });
