352330e1a3ab7fbc2a8a2e73503031b78a060d9e
timconceivable <headskape@yahoo.com>
2023-10-31 13:58:07 -0500

    refining rotation & adding fuel and chat updates

--- dom-and-events/studio/scripts.js
+++ dom-and-events/studio/scripts.js
@@ -3,6 +3,8 @@ function init () {
     const landingButton = document.getElementById("landing");
     const abortButton = document.getElementById("missionAbort");
     const flightStatus = document.getElementById("flightStatus");
+    const astroChat = document.getElementById("chat");
+    const fuelTank = document.getElementById("fuel");
     const shuttleHeight = document.getElementById("shuttleHeight");
     const shuttleBg = document.getElementById("shuttleBackground");
     const shuttle = document.getElementById("rocket");    
@@ -11,19 +13,28 @@ function init () {
     const leftButton = document.getElementById("left");
     const rightButton = document.getElementById("right");
     
-    const increment = 10;
+    const center = "90px";
+    const shuttleSpeed = 10;
     let inAir = false;
     let currentXPos = parseInt(getComputedStyle(shuttle).left);
     let currentYPos = parseInt(getComputedStyle(shuttle).bottom);
-    let rotation = parseInt(getComputedStyle(shuttle).rotate);
+    let fuelLevel = fuelTank.innerHTML;
     
+    function useFuel() {
+        fuelLevel -= 10000;
+        return fuelLevel;
+    }
+
     takeoffButton.addEventListener("click", function() {
         if (window.confirm("Confirm that the shuttle is ready for takeoff")) {
             flightStatus.innerHTML = "Shuttle in flight";
+            astroChat.innerHTML = "Houston, we have liftoff!"
+            fuelLevel = 500000;
+            fuelTank.innerHTML = fuelLevel;
             shuttleBg.style.backgroundColor = "blue";
-            shuttleHeight.innerHTML = 10000;
+            shuttleHeight.innerHTML = 1000*shuttleSpeed;
             shuttle.style.bottom = "10px";
-            shuttle.style.left = "180px";
+            shuttle.style.left = center;
             inAir = true;
             currentXPos = parseInt(getComputedStyle(shuttle).left);
             currentYPos = parseInt(getComputedStyle(shuttle).bottom);
@@ -34,10 +45,13 @@ function init () {
     landingButton.addEventListener("click", function () {
         window.alert("The shuttle is landing. Landing gear engaged.")
         flightStatus.innerHTML = "The shuttle has landed.";
+        astroChat.innerHTML = "Home safe."
+        fuelLevel = 500000;
+        fuelTank.innerHTML = fuelLevel;
         shuttleBg.style.backgroundColor = "green";
         shuttleHeight.innerHTML = 0;
         shuttle.style.bottom = "0";
-        shuttle.style.left = "180px";
+        shuttle.style.left = center;
         inAir = false;
         currentXPos = parseInt(getComputedStyle(shuttle).left);
         currentYPos = parseInt(getComputedStyle(shuttle).bottom);
@@ -47,10 +61,11 @@ function init () {
     abortButton.addEventListener("click", function () {
         if (window.confirm("Confirm that you want to abort the mission.")) {
             flightStatus.innerHTML = "Mission aborted.";
+            astroChat.innerHTML = "ABORT! ABORT!"
             shuttleBg.style.backgroundColor = "red";
             shuttleHeight.innerHTML = 0;
             shuttle.style.bottom = "0";
-            shuttle.style.left = "180px";
+            shuttle.style.left = center;
             inAir = false;
             currentXPos = parseInt(getComputedStyle(shuttle).left);
             currentYPos = parseInt(getComputedStyle(shuttle).bottom);
@@ -59,36 +74,48 @@ function init () {
     });
 
     upButton.addEventListener("click", function () {
-        if (currentYPos < 371 && inAir) {
-            currentYPos += increment;
+        if (currentYPos < 171 && inAir) {
+            astroChat.innerHTML = "Weeeeeee!"
+            currentYPos += shuttleSpeed;
             shuttle.style.bottom = String(currentYPos)+"px";
-            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML)+(1000*increment);
+            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML)+(1000*shuttleSpeed);
             shuttle.style.rotate = "0deg";
+            fuelTank.innerHTML = useFuel();
         }
     });
 
     downButton.addEventListener("click", function () {
         if (currentYPos > 9 && inAir) {
-            currentYPos -= increment;
+            astroChat.innerHTML = "Weeeeeee!"
+            currentYPos -= shuttleSpeed;
             shuttle.style.bottom = String(currentYPos)+"px";
-            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML)-(1000*increment);
+            shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML)-(1000*shuttleSpeed);
             shuttle.style.rotate = "180deg";
+            fuelTank.innerHTML = useFuel();
         }
     });
 
     rightButton.addEventListener("click", function () {
-        if (currentXPos < 381 && inAir) {
-            currentXPos += increment;
+        if (currentXPos < 181 && inAir) {
+            astroChat.innerHTML = "Weeeeeee!"
+            currentXPos += shuttleSpeed;
             shuttle.style.left = String(currentXPos)+"px";
-            shuttle.style.rotate = "45deg";
+            if (parseInt(getComputedStyle(shuttle).rotate) > 45 || parseInt(getComputedStyle(shuttle).rotate) < -45) {
+                shuttle.style.rotate = "135deg";    
+            } else shuttle.style.rotate = "45deg";
+            useFuel();
         }
     });
     
     leftButton.addEventListener("click", function () {
         if (currentXPos > -1 && inAir) {
-            currentXPos -= increment;
+            astroChat.innerHTML = "Weeeeeee!"
+            currentXPos -= shuttleSpeed;
             shuttle.style.left = String(currentXPos)+"px";
-            shuttle.style.rotate = "-45deg";
+            if (parseInt(getComputedStyle(shuttle).rotate) > 45 || parseInt(getComputedStyle(shuttle).rotate) < -45) {
+                shuttle.style.rotate = "-135deg";
+            } else shuttle.style.rotate = "-45deg";
+            fuelTank.innerHTML = useFuel();
         }
     });
 }
