7d9f5ce02abf63f27e2f75a98d51c6613643ef03
timconceivable <headskape@yahoo.com>
2023-11-05 12:44:41 -0600

    attempting to resolve merge conflicts with LC main repo by copy/pasting changed lines

--- arrays/exercises/part-one-arrays.js
+++ arrays/exercises/part-one-arrays.js
@@ -1,4 +1,4 @@
-//Create an array that can hold 4 items name practiceFile.
+//Create an array called practiceFile with the following entry: 273.15
 let practiceFile = [273.15,"","",""];
 
 //Use the bracket notation method to add "42" and "hello" to the array. Add these new items one at a time.  Print the array after each step to confirm the changes.
@@ -9,6 +9,6 @@ console.log(practiceFile);
 practiceFile[3] = "poo poo";
 console.log(practiceFile);
 
-//Use a SetValue to add the items "false", and "-4.6" to the array.  Print the array to confirm the changes.
+//Use a single .push() to add the following items: false, -4.6, and "87".  Print the array to confirm the changes.
 practiceFile.push(false, -4.6, "87");
 console.log(practiceFile);
