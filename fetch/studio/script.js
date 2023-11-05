83e49c4d1aeac18aff00d1da23baffd64a67c3b0
timconceivable <headskape@yahoo.com>
2023-11-05 09:33:38 -0600

    fixing styling

--- fetch/studio/script.js
+++ fetch/studio/script.js
@@ -1,7 +1,8 @@
-function init () {
+window.addEventListener("load", function () { // AFTER PAGE LOADS
+   // FETCH ASTRONAUT DATA
    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then( function(response) {
       response.json().then( function(json) {
-         const div = document.getElementById('container');
+         // CREATE AND SORT ASTRONAUT LIST
          astronautList = [];
          for (i in json) {
             astronautList.push(json[i]);
@@ -12,30 +13,38 @@ function init () {
          const title = document.getElementById("title");
          title.innerHTML += `${astronautList.length}`;
          
+         // GENERATE HTML CONTENT (ASTRONAUT BIOS)
+         const container = document.getElementById("container");
          for (i in astronautList) {
             let astronaut = astronautList[i];
             let isActive = astronaut.active ? "active" : "inactive";
             container.innerHTML += `
             <div class="astronaut">
                <div class="bio">
-                  <div class="names">
-                     <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
-                  </div>
-                  <div class="info">
-                     <ul>
-                        <span class="${isActive}"> ${isActive.toUpperCase()}</span>
-                        <li><strong>Hours in space:</strong> ${astronaut.hoursInSpace}</li>
-                        <li><strong>skills:</strong> ${astronaut.skills}</li>
-                     </ul>
-                  </div>
+                  <h3 class="names">${astronaut.firstName} ${astronaut.lastName}</h3>
+                  <ul class="info">
+                     <span class="${isActive}"> ${isActive.toUpperCase()}</span>
+                     <li><strong>Hours in space:</strong>&nbsp; ${astronaut.hoursInSpace}</li>
+                     <li><strong>Skills:</strong>&nbsp; ${String(astronaut.skills).replaceAll(',', ', ')}</li>
+                  </ul>
                </div>
                <img class="avatar" src="${astronaut.picture}">
             </div>
             `;
          }
-         
        });
    });
-}
 
-window.addEventListener("load", init);
+   // GENERATE BACKGROUND STARS
+   const body = document.getElementById("body");
+   for (let i=0; i<100; i++) {
+      body.innerHTML += '<span class="star">⭐</span>'; //✨
+   }
+   const stars = document.getElementsByClassName("star");
+   for (let index in stars) {
+      stars[index].style.top = `${Math.random()*150}%`;
+      stars[index].style.left = `${Math.random()*100}%`;
+      stars[index].style.fontSize = `${Math.ceil(Math.random()*10)}px`;
+      stars[index].style.textShadow = `0px 0px 5px ghostwhite, 0px 0px ${Math.random()*30}px yellow`;
+   }
+});
